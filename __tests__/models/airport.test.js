8e39a80113b4c0c53015690f826d9ea1d8dbfe68
Neil Sweeney <neil@wolfiezero.com>
2020-03-09 23:35:18 +0000

    Update tests for airports

--- __tests__/models/airport.test.js
+++ __tests__/models/airport.test.js
@@ -1,13 +1,47 @@
-const Airport = require('../../src/models/airport.model');
+/* eslint-env jest */
+const { AirportModel } = require('../../src/models');
 
 describe('airport.model', () => {
-  test('Query airports table', async done => {
-    const results = await Airport.findAll({
-      where: {
-        iata_code: 'LHR',
-      },
-    });
-    console.log(results);
+  const airportObject = expect.objectContaining({
+    id: expect.any(Number),
+    iata_code: expect.any(String),
+    ident: expect.any(String),
+    iso_country: expect.any(String),
+    municipality: expect.any(String),
+    continent: expect.any(String),
+    coordinates: expect.any(String),
+  });
+
+  let randomAirport = {};
+
+  test('Get all airports', async done => {
+    const result = await AirportModel.findAll();
+    const resultSize = result.length;
+    const index = Math.floor(Math.random() * resultSize);
+    randomAirport = result[index];
+    expect(resultSize).toBeGreaterThan(1000);
+    expect(result[index]).toEqual(airportObject);
+    done();
+  });
+
+  test('Get airport using `getAirportByIdentifier()` with `iata_code`', async done => {
+    const result = await AirportModel.getAirportByIdentifier('JFK');
+    expect(result.ident).toBe('KJFK');
+    expect(result).toEqual(airportObject);
+    done();
+  });
+
+  test('Get airport using `getAirportByIdentifier()` with `ident`', async done => {
+    const result = await AirportModel.getAirportByIdentifier('CYUL');
+    expect(result.iata_code).toBe('YUL');
+    expect(result).toEqual(airportObject);
+    done();
+  });
+
+  test('Get airport using `getAirportByIdentifier()` with `id`', async done => {
+    const result = await AirportModel.getAirportByIdentifier(randomAirport.id);
+    expect(result.iata_code).toEqual(randomAirport.iata_code);
+    expect(result).toEqual(airportObject);
     done();
   });
 });
--- __tests__/services/sequelize.test.js
+++ __tests__/services/sequelize.test.js
@@ -1,9 +1,10 @@
+/* eslint-env jest */
 const sequelize = require('../../src/services/sequelize.service');
-const Airport = require('../../src/models/airport.model');
 
 describe('sequelize.service', () => {
   test('It can connect to the database', async done => {
-    const results = await sequelize.authenticate()
+    const results = await sequelize
+      .authenticate()
       .then(() => true)
       .catch(() => false);
     expect(results).toBeTruthy();
