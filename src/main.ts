8b06e9d5e408db4636250ea00674759e02cf5b35
Neil Sweeney <neil@wolfiezero.com>
2020-10-01 00:39:02 +0100

    Update Swagger definitions

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle(process.env.npm_package_title || '')
+    .setDescription(process.env.npm_package_description || '')
+    .setVersion(process.env.npm_package_version || '')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
